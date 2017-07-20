fb7ae86dfc7a3c5906f1552221e38f26f7463976
gkrishnan724 <gkrishnan724@gmail.com>
2017-07-20 20:47:26 +0530

    Updates

---
+++ scripts/mifosXComponents.68cf1d11.js
@@ -0,0 +1,39 @@
+define(['Q', 'underscore', 'mifosX'], function (Q) {
+    var components = {
+        models: [
+            'models.3d5c68d7'
+        ],
+        services: [
+            'ResourceFactoryProvider',
+            'HttpServiceProvider',
+            'AuthenticationService',
+            'SessionManager',
+            'Paginator',
+            'UIConfigService',
+            'NotificationResponseHeaderProvider'
+        ],
+        controllers: [
+            'controllers.92171add'
+        ],
+        filters: [
+            'filters.34474998'
+        ],
+        directives: [
+            'directives.dffbf193'
+        ]
+    };
+
+    return function() {
+        var defer = Q.defer();
+        require(_.reduce(_.keys(components), function (list, group) {
+            return list.concat(_.map(components[group], function (name) {
+                return group + "/" + name;
+            }));
+        }, [
+            'routes-initialTasks-webstorage-configuration.0d972d51'
+        ]), function(){
+            defer.resolve();
+        });
+        return defer.promise;
+    }
+});
